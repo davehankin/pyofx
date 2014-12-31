8dacfbf2fd06d492056a538f2674ba5c8cfb7535
Steven Rossiter <steve@flexsis.co.uk>
2014-12-31 12:54:41 +0000

    fix tracked bat file

--- build_all.bat
+++
@@ -1,7 +0,0 @@
-C:\Python27\python.exe setup.py bdist_egg upload --identity="Steven Rossiter" --sign --quiet
-C:\Python34\python.exe setup.py bdist_egg upload --identity="Steven Rossiter" --sign --quiet
-C:\Python27\python.exe setup.py bdist_wininst --target-version=2.7 register upload --identity="Steven Rossiter" --sign --quiet
-C:\Python34\python.exe setup.py bdist_wininst --target-version=3.4 register upload --identity="Steven Rossiter" --sign --quiet
-C:\Python34\python.exe setup.py sdist upload --identity="Steven Rossiter" --sign
-C:\Python27\python.exe setup.py sdist upload --identity="Steven Rossiter" --sign
-pause
